5f42920b5ab3b3a2aa886908d37e8444fe7a31cc
Rose1845 <odhiamborose466@gmail.com>
2022-10-02 11:12:20 +0300

    updated blogs page

---
+++ src/components/Blog.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Blog = () =>{
+  return (
+    <div className='blog-section'>
+      <div className='title'>
+        <h1>Our<br/><span>Green</span>{""}Blogs
+        </h1>
+      </div>
+      <div className='blogContent'>
+        <div className='first-blog'>
+            <div>
+                <h2>Why we should produce more off-grid power</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci praesentium et repellat? Dolorum nobis repellendus adipisci. Natus reiciendis porro nihil!</p>
+                <button>READ MORE</button>
+            </div>
+            <div>
+                <div>
+                    <h2>Industry Academia Platform Project</h2>
+                </div>
+                <div>
+                    <h2>Picking the right arduino</h2>
+                </div>
+            </div>
+        </div>
+        <div className='second-blog'>
+            <div>
+                <h3>Microsoft Learn Student Ambassador</h3>
+            </div>
+            <div>
+             <h3>Can I use arduino commercially</h3>
+            </div>
+            <div>
+              <h3>Discover internet of things</h3>
+            </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
